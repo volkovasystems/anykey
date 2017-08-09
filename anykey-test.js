bbc291b64cb0001b322916fe469ed56a716b13c8
volkovasystems <richeve.bebedor@gmail.com>
2017-08-10 03:52:54 +0800

    Improved test.
    Updated build.

--- anykey-test.js
+++ anykey-test.js
@@ -8,14 +8,18 @@ assert.equal( anykey( "toString", 123 ), true, "should be equal to true" );
 
 assert.equal( anykey( [ "toString", "valueOf" ], 123 ), true, "should be equal to true" );
 
-/* These test cases have changed from true to false
 assert.equal( anykey( Symbol.for( "property" ), { [ Symbol.for( "property" ) ]: "value" } ), true, "should be equal to true" );
 
 assert.equal( anykey( "property", { "property": "value" } ), true, "should be equal to true" );
-*/
+
 
 assert.equal( anykey( [ "toString", "valueOf", "yeah" ], false ), true, "should be equal to true" );
 
 assert.equal( anykey( [ "hello", "world", "yeah" ], "" ), false, "should be equal to false" );
+
+var hello = Symbol( "hello" );
+var hi = Symbol( "hi" );
+var object = { [ hello ]: 123, [ hi ]: 123 };
+assert.equal( anykey( [ hello, hi ], object ), true, "should be true" );
 
 console.log( "ok" );
